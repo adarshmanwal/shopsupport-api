7983273795c1598ccc2c898f79ac969f39cad18d
adarshmanwal <adarshmanwal9@gmail.com>
2024-05-29 22:28:45 +0530

    login signup achived with token and cors

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,12 +6,20 @@ exports.register = async (req, res) => {
   const { name, email, password, usertype } = req.body;
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
-    const user = await User.create({
+    let user = await User.create({
       name,
       email,
       password: hashedPassword,
       usertype,
     });
+    user = await User.findOne({ where: { email } });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    user.token = token
     res.status(201).json(user);
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -28,7 +36,8 @@ exports.login = async (req, res) => {
     const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
       expiresIn: "1h",
     });
-    res.json({ token });
+    user.dataValues.token = token
+    res.json(user);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const port = process.env.PORT || 3000;
 const config = require("./config/config.js");
 const routes = require("./routes");
 const bodyParser = require('body-parser');
+var cors = require('cors')
 
 app.use(bodyParser.json());
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
 // Test the database connection
 const sequelize = new Sequelize(config.development);
