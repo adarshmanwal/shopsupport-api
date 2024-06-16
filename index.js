5473d427d8944f492e1b00275292164835fb055d
adarshmanwal <adarshmanwal9@gmail.com>
2024-06-16 17:48:28 +0530

    file upload sucessfuly

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ const routes = require("./routes");
 const bodyParser = require('body-parser');
 var cors = require('cors')
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit:'50mb'})); 
 app.use(cors())
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended:true, limit:'50mb'})); 
+
 // Test the database connection
 const sequelize = new Sequelize(config.development);
 sequelize
--- routes/shopRoutes.js
+++ routes/shopRoutes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const shopController = require('../controllers/shopController'); // Import shop controller
+const { promisify } = require('util'); // Import promisify from util module
+const multer = require('multer');
+const path = require('path');
 
 const authenticateToken = (req, res, next) => {
   const token = req.header('Authorization')?.split(' ')[1];
@@ -16,11 +19,35 @@ const authenticateToken = (req, res, next) => {
   }
 };
 
+// Set up Multer storage and file handling
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); // Directory to save the uploaded files
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname)); // Use a unique filename
+  },
+});
+
+const upload = multer({ storage: storage }).array('images', 10);
+const uploadAsync = promisify(upload); // Promisify multer's async function
+
 // Define shop routes
 router.post('/', authenticateToken, shopController.create);
 router.get('/:id', authenticateToken, shopController.getById);
 router.get('/', authenticateToken, shopController.getAll);
 router.put('/:id', authenticateToken, shopController.update);
 router.delete('/:id', authenticateToken, shopController.delete);
+router.post('/upload', authenticateToken, async (req, res) => {
+  try {
+    await uploadAsync(req, res); // Handle file upload
+
+    // Extract filenames from req.files
+    const fileNames = req.files.map(file => file.filename);
+    res.status(200).json(fileNames);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
 
 module.exports = router;
